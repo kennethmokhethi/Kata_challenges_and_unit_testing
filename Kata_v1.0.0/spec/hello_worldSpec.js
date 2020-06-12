8743b4aee91f34a2bdc669bebd4114b11ceb274a
Kennethmokhethi <kennethmahlatsemokhethi@gmail.com>
2020-06-12 02:04:52 +0200

    Added structure for implementation of tests and test

--- Kata_v1.0.0/spec/hello_worldSpec.js
+++ Kata_v1.0.0/spec/hello_worldSpec.js
@@ -1,7 +1,19 @@
 let hello_world = require("../src/hello_world");
 
-describe("Testing", () => {
-  it("Return Test successful", () => {
+/*
+describe("Initials_KC_UniqueIntegerForTheKataChallengeSuite_T eg GK_KC13", () => {
+ 
+  it("Short descriptive deatils,YourInitials_KCT{unique_number_for_this_test} eg.GK_KCT05 , () => {
+   
+      TEST IMPLEMENTATION
+
+
+  });
+});
+*/
+
+describe("KMM_KC00", () => {
+  it("Return Test successful:KMM_KCT01", () => {
     expect(hello_world()).toBe("Test successful");
   });
 });
--- Kata_v1.0.0/src/hello_world.js
+++ Kata_v1.0.0/src/hello_world.js
@@ -1,6 +1,27 @@
+/*
+-------------------------------------------------------------------------------indicates state of the kata challenge
+YourInitials_KC_UniqueIntegerForTheKataChallengeSuite:KataChallengeUrl eg GK_KC01:http://codekata.com/
+
+
+FUNCTION IMPLEMENTATION{
+YourInitials_KC{unique_number_for_this_challenge}:TestCaseIdentifie eg.KMM_KC01:GM_KCT05
+}
+
+
+
+-------------------------------------------------------------------------------indicates end of kata challenge
+
+*/
+
+/*   
+--------------------------------------------------------------------------------
+KMM_KC00:http://helloworld.com
+*/
 const hello_world = () => {
+  //KMM_KC01:KMM_KCT01
   return "Test successful";
 };
 
-module.exports = hello_world;
+//------------------------------------------------------------------------------
 
+module.exports = hello_world;
