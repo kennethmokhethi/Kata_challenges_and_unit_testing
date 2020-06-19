9d6e9c91a8cf8d49ee1d3d187438b6f99f8a856d
Kennethmokhethi <kennethmahlatsemokhethi@gmail.com>
2020-06-19 04:07:27 +0200

    Added getCount function

--- Kata_v1.0.0/src/kata_v2.js
+++ Kata_v1.0.0/src/kata_v2.js
@@ -1,12 +1,22 @@
 function comp(array1, array2) {
-    var comp1 = [];
-    for (var j in array2) {
-      comp1.push(Math.sqrt(array2[j]));
-    }
-    let resultsActions = array1.map(d => d);
-    return comp1.every(i => resultsActions.includes(i));
+  var comp1 = [];
+  for (var j in array2) {
+    comp1.push(Math.sqrt(array2[j]));
   }
-  
+  let resultsActions = array1.map((d) => d);
+  return comp1.every((i) => resultsActions.includes(i));
+}
+
 //   console.log(comp( [79, 32, 54, 34, 3, 34, 45],  [6241, 1024, 2917, 1156, 9, 1156, 2025]));
 
-  module.exports = comp;
+const getCount = (str) => {
+  //KMM_KC01:https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/javascript
+  return str.split("").filter(isVowel).length;
+};
+
+const isVowel = (characterElement) => {
+  return characterElement.match(/[aeiou]/g);
+};
+
+
+module.exports = comp;
